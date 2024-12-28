6d6a3dacdf320e3550791a2d4debf7ed19d93138
jair do Nascimento de Oliveira <jnojair2010@gmail.com>
2024-12-28 07:15:11 -0300

    create material-ui-insert

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:{
+    include:['@emotion/styled']
+  },
+ 
  /* resolve:{
     alias:{
       '@':path.resolve(__dirname, './src')
