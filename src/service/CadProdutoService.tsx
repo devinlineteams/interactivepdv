2f22e61bb09d7985861a16a0c7a65ff87db163b8
jair do Nascimento de Oliveira <jnojair2010@gmail.com>
2025-03-23 20:22:30 -0300

    atualizado função de cadastro de produto

---
+++ src/service/CadProdutoService.tsx
@@ -0,0 +1,10 @@
+import { CadProduto } from "@/model/Cad-Producto";
+
+class CadProdutoservice{
+
+    postarProdutoBd(prod:CadProduto){
+        console.log(prod)
+    }
+
+}
+export default CadProdutoservice;
